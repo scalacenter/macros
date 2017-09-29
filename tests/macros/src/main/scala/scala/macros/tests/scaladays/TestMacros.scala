57cc2082f8e9fe97e334a49b1bd3fd628801a701
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-29 10:31:37 +0200

    Move language import to scalacOptions.
    This is necessary for to cross-build macros across compilers.
    The scalac-plugin SyntaxAnalyzer inserts `import
    scala.language.exprimental.macros` but it doesn't seem to be picked up by
    scalac for all macros, only the Serialize macro.

--- tests/macros/src/main/scala/scala/macros/tests/scaladays/TestMacros.scala
+++ tests/macros/src/main/scala/scala/macros/tests/scaladays/TestMacros.scala
@@ -2,8 +2,6 @@ package scala.macros.tests.scaladays
 
 import scala.macros._
 
-import scala.language.experimental.macros
-
 object TestMacros {
   def syntax[T](a: T): String = macro Lit.String(a.syntax)
   def structure[T](a: T): String = macro Lit.String(a.structure)
