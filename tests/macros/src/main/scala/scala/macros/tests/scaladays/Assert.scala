6750d36411703a7e0f79277ccb39142fb1fd997c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-10-04 14:57:20 +0200

    WIP

---
+++ tests/macros/src/main/scala/scala/macros/tests/scaladays/Assert.scala
@@ -0,0 +1,20 @@
+package scala.macros.tests.scaladays
+
+import scala.macros._
+
+object Assert {
+  def assert(cond: Boolean): Unit = macro {
+    val root = Term.Name
+      .apply("_root_")
+      .select("org")
+      .select("junit")
+      .select("Assert")
+    println(cond.structure)
+    cond match {
+      case Term.Apply(Term.Select(qual, Term.Name("==")), arg :: Nil) =>
+        root.select("assertEquals").apply(qual :: arg :: Nil)
+      case _ =>
+        root.select("assertTrue").apply(cond :: Nil)
+    }
+  }
+}
